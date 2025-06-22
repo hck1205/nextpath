a390d25cb8a56c2991ec5a289c112438c146afa0
hck1205@daum.net <hck1205@daum.net>
2025-06-22 10:00:04 +0900

    add: foundation for basic route styles

---
+++ src/App.styled.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Nav = styled.nav`
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+  background: #eee;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
+
+// import * as S from './App.styled';
+import './assets/normalize.css';
+
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <S.Nav>
+        <Link to="/">Home</Link>
+        <Link to="/simulator">Simulator</Link>
+        <Link to="/about">About</Link>
+      </S.Nav> */}
+      <Suspense fallback={<CircularProgress />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
